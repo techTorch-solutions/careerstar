8415fbca763c2550edf4ac483de8b4fa2b2c596f
dhirendra <shindedhirendra780@gmail.com>
2024-03-12 00:16:55 +0530

    first page complete , no resposive ,need more animations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import NavLinks from './Links'
 const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
-        <header className={`ease-in-out duration-700 font-poppins flex justify-around items-center text-white top-0 sticky flex-wrap ${open ? 'min-h-72' : 'min-h-20'} bg-gradient-to-b from-start to-customTeal`}>
+        <header className={`ease-in-out duration-700 font-poppins flex justify-around items-center text-white top-0 sticky flex-wrap ${open ? 'min-h-72' : 'min-h-20'} bg-gradient-to-b from-start to-customTeal z-50`}>
             <div>
                 <h1 className='text-2xl font-semibold'>Career Star</h1>
             </div>
