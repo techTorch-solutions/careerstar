1b70280dfa7c2ade8cda378425a75f6aa4c7e654
BlockbusterAndy <124876077+BlockbusterAndy@users.noreply.github.com>
2024-03-18 21:32:00 +0530

    minor changes
    minor changes

---
+++ src/components/Cards.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+export default function Cards(props) {
+  const data1 = props.data
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <div className='w-3/4 m-auto'>
+      <div className='md:hidden'>
+        <Slider {...settings}>
+          {data1.map((d, key) => (
+            <div className='flex justify-center'>
+              <div key={key} className={`${d.theme === 'blue' ? 'bg-[#9CE7FF]' : 'bg-[#F6FFFD]'} mt-8 relative left-4 text-left w-[90%] flex flex-col gap-y-3.5 px-4 py-5 rounded-2xl justify-between`} id='card'>
+                <div className=''>
+                  <h1 className='text-[20px] font-bold my-5 mx-2 text-center'>{d.title}</h1>
+                  <p className='font-ubuntu font-light text-[13px] '>{d.text}</p>
+                </div>
+                <button className={`${d.theme === 'blue' ? 'bg-[#FCF9DB]' : 'bg-[#9CE7FF]'} p-2 font-bold text-sm rounded-2xl my-4`}>Learn More</button>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      <div className='hidden md:flex'>
+        {/* Render non-slider content here for larger screens */}
+        {data1.map((d, key) => (
+          <div className='flex'>
+            <div key={key} className={`${d.theme === 'blue' ? 'bg-[#9CE7FF]' : 'bg-[#F6FFFD]'} text-left w-[90%] flex flex-col gap-y-3.5 px-10 py-5 rounded-2xl justify-between`} id='card'>
+              <div className=''>
+                <h1 className='text-[20px] font-bold my-5 mx-2 text-center'>{d.title}</h1>
+                <p className='font-ubuntu font-light text-[13px] '>{d.text}</p>
+              </div>
+              <button className={`${d.theme === 'blue' ? 'bg-[#FCF9DB]' : 'bg-[#9CE7FF]'} p-4 font-bold text-lg rounded-2xl my-4`}>Learn More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        start: '#000 0%',
+        customTeal: '#51C4E8 350%',
+        buttons: '#33A09A'
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+        ubuntu: ['Ubuntu']
+      }
+    },
+  },
+  plugins: [],
+}
+
